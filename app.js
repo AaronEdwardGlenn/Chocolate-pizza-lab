6a0a1f7a68548084f930b5334144b6e3af91aec9
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2019-10-14 12:12:32 -0700

    final

---
+++ app.js
@@ -0,0 +1,97 @@
+const lineOut1 = document.querySelector('#li1'); 
+
+
+lineOut1.addEventListener('change', function() {
+    
+    lineOut1.style.textDecoration = 'line-through'; 
+
+});  
+
+const lineOut2 = document.querySelector('#li2'); 
+
+
+lineOut2.addEventListener('change', function() {
+    
+    lineOut2.style.textDecoration = 'line-through'; 
+
+});  
+
+const lineOut3 = document.querySelector('#li3'); 
+
+
+lineOut3.addEventListener('change', function() {
+    
+    lineOut3.style.textDecoration = 'line-through'; 
+
+});  
+
+const lineOut4 = document.querySelector('#li4'); 
+
+
+lineOut4.addEventListener('change', function() {
+    
+    lineOut4.style.textDecoration = 'line-through'; 
+
+});  
+
+const lineOut5 = document.querySelector('#li5'); 
+
+
+lineOut5.addEventListener('change', function() {
+    
+    lineOut5.style.textDecoration = 'line-through'; 
+
+});  
+
+const lineOut6 = document.querySelector('#li6'); 
+
+
+lineOut6.addEventListener('change', function() {
+    
+    lineOut6.style.textDecoration = 'line-through'; 
+
+});  
+
+const lineOut7 = document.querySelector('#li7'); 
+
+
+lineOut7.addEventListener('change', function() {
+    
+    lineOut7.style.textDecoration = 'line-through'; 
+
+});  
+
+let lineOut8 = document.querySelector('#li8'); 
+lineOut8.addEventListener('change', function() {
+    
+    lineOut8.style.textDecoration = 'line-through'; 
+
+});  
+
+let lineOut9 = document.querySelector('#li9'); 
+lineOut9.addEventListener('change', function() {
+    
+    lineOut9.style.textDecoration = 'line-through'; 
+
+});  
+
+let lineOut10 = document.querySelector('#li10'); 
+lineOut10.addEventListener('change', function() {
+    
+    lineOut10.style.textDecoration = 'line-through'; 
+
+});  
+
+let lineOut11 = document.querySelector('#li11'); 
+lineOut11.addEventListener('change', function() {
+    
+    lineOut11.style.textDecoration = 'line-through'; 
+
+});  
+
+let lineOut12 = document.querySelector('#li12'); 
+lineOut12.addEventListener('change', function() {
+    
+    lineOut12.style.textDecoration = 'line-through'; 
+
+});  
